e615b5661748ffec61174f0337e9e3b2781ed6c2
OrangeJuiceGuy1 <ericlin1253@gmail.com>
2025-05-21 22:59:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-
-    const timerText = document.getElementById("timertext");
-    let holdTime = null;
-    let timeRunning = false;
-    let currentTime = 0;
-    let interval = null;
-
-    let debounce = false;
-    let timeList = {};
-
-
-    document.addEventListener("keydown", function(event) {
-
-        if (event.code === "Space" && debounce === false) {
-            if (timeRunning === false) {
-                debounce = true; 
-                timerText.style.color = "red";
-
-                holdocument.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
     const timerText = document.getElementById("timertext");
     let holdTime = null;
@@ -75,46 +55,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
             debounce = false;
         }
-    });
-});
-                    timerText.style.color = "green";
-                    holdTime = null;
-                },300);
-
-
-            } else {
-                timerText.style.color = "black"
-                timeRunning = false
-            };
-
-
-        };
-
-    });
-
-    document.addEventListener("keyup", function(event) {
-        if (event.code === "Space") {
-
-
-            if (timerText.style.color === "green") {
-                timeRunning = true;
-                
-
-
-            } else {
-                if (holdTime !== null) {
-                    clearTimeout(holdTime);
-                    holdTime = null;
-                };
-                // if (timerText.style.color === "green") {
-                //   console.log("Green!");  
-                // };
-
-                debounce = false;
-                timerText.style.color = "black";
-            };
-            
-        } 
-
     });
 });
